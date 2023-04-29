dc06046e067244093ceed9a33b5d59113a8c4ad7
samuelalecas <samuelalecas@gmail.com>
2023-04-29 09:04:17 +0200

    Resolve problem

--- 004.js
+++ 004.js
@@ -4,8 +4,32 @@
 // Find the largest palindrome made from the product of two n-digit numbers.
 
 function largestPalindromeProduct(n) {
+  let maxDigits = 10 ** n;
+  let number = 0;
+  let largestPalindrome = 0;
 
-    return true;
+  for (let num1 = maxDigits - 1; num1 > 0; num1--) {
+    for (let num2 = maxDigits - 1; num2 > 0; num2--) {
+      number = num1 * num2;
+      if (isPalindrome(number)) {
+        if (number > largestPalindrome) {
+          largestPalindrome = number;
+        }
+      }
+    }
   }
-  
-  largestPalindromeProduct(3);
+
+  return largestPalindrome;
+}
+
+function isPalindrome(number) {
+  let currentNumber = number.toString();
+  let numberReversed = currentNumber.split('').reverse().join('');
+  return currentNumber === numberReversed;
+}
+
+
+// console.log(largestPalindromeProduct(3));
+
+largestPalindromeProduct(3);
+
